fe3d50e30dbc2cf0572ac3c25a3049161ad1ed8b
therohantomar <therohantomar@gmail.com>
2023-09-19 18:04:24 +0530

    responsiveness & category added

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -5,17 +5,19 @@ import { AiFillStar } from "react-icons/ai";
 function Product() {
   const { id } = useParams();
   const Product = useProduct(id);
-
-  const { title, price, rating, description, image } = Product;
+  console.log(Product)
+  // destructing object
+  const { title, price, rating, description, image, category } = Product;
 
   if (Product.length === 0) return <h1>loading.......</h1>;
 
   return (
     <div className="flex gap-10 p-4 flex-wrap  h-full items-center w-full justify-center">
       <img src={image} alt={title} className="w-96  h-max border-2 p-4" />
-      <div className="w-1/2  h-max">
+      <div className="xl:w-1/2 2xl:w-1/2 lg:w-full md:w-full sm:w-full  h-max">
         <h1 className="h-20 font-bold text-xl">{title}</h1>
-        <p className="h-40 w-2/3 font-thin">{description}</p>
+        <p className="h-40 xl:w-2/3 2xl:w-2/3 lg:w-full md:w-full sm:w-full  font-thin">{description}</p>
+        <p>{category?.toUpperCase()}</p>
         <span>
           <div className="flex w-max gap-20 my-10 items-center">
             {" "}
