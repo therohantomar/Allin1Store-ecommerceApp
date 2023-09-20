57c3ab0b64c6e89d0d15bfa5a60efb909733631d
therohantomar <therohantomar@gmail.com>
2023-09-20 19:04:09 +0530

    babel added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.js";
+import App from "./App.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
