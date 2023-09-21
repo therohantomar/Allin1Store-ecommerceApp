86e40591da5ee54e55996dfc0490781de19566c2
therohantomar <therohantomar@gmail.com>
2023-09-21 22:08:54 +0530

    cart page created

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,26 +1,55 @@
-import { useSelector } from "react-redux"
-
+import { useSelector } from "react-redux";
+import { BsTrash } from "react-icons/bs";
 
 function Cart() {
-  const Products=useSelector(store=>store.cart.items)
+  const Products = useSelector((store) => store.cart.items);
+
+  
+
+
+
   return (
-    <div>
-    <h1>You have {Products.length==0?0:Products.length} product in your cart</h1>
-      {
-        Products?.map(product=>{
-         return (<div key={product.id} className="flex gap-8">
-          <img src={product.image} alt={product?.title} className="w-40 h-32 "/>
-           <span>
-            <h1>{product.title}</h1>
-            <h2>$ {product.price}</h2>
-            <h3>quantity {product.quantity}</h3>
-            <button className="px-2 border-2 border-black mt-2 hover:bg-black  hover:text-white transition-all ">Remove</button>
-            </span>
-          </div>)
-        })
-      }
+    <div className="flex flex-wrap h-max my-10 justify-around ">
+      <main>
+        {Products?.map((product) => {
+          return (
+            <div
+              key={product.id}
+              className="flex flex-wrap   h-max p-2 border-2 w-max my-2  items-center gap-8"
+            >
+              <img
+                src={product.image}
+                alt={product?.title}
+                className="w-40 h-32 "
+              />
+              <span className="flex flex-col items-center gap-4 ">
+                <h1 className="w-52">{product.title}</h1>
+                <h2>$ {product.price}</h2>
+                <span className=" flex items-center  text-xl  gap-2">
+                  <button className="hover:bg-black hover:text-white   border border-black px-2  text-xl">
+                    -
+                  </button>
+                  <h3>quantity: {product.quantity}</h3>
+                  <button className="hover:bg-black hover:text-white  border border-black px-2  text-xl">
+                    +
+                  </button>
+                </span>
+                <button className="px-2 border-2 flex border-black  hover:bg-black items-center h-8  w-max hover:text-white transition-all ">
+                  Remove <BsTrash />
+                </button>
+              </span>
+
+            </div>
+          );
+        })}
+      </main>
+      <span className="my-10">
+        <h1>Total Items- {Products.length}</h1>
+        <button className="px-4 border-2 hover:bg-black hover:text-white ">Checkout</button>
+      </span>
+
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
