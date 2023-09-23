c38981dc8d0c1b5c665b31c8ea56dce5c6082e6a
therohantomar <therohantomar@gmail.com>
2023-09-23 22:26:03 +0530

    location.address.city? ternary loaded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
 
   return (
     <header className="w-full  h-max sticky top-0 left-0 right-0 z-20 border-b-2  bg-white flex justify-around ">
-      <span className="font-bold text-lg my-4">@llin1 <h1 className="text-sm text-gray-400 font-thin">{location?.address.county?location?.address.county:location?.address.city}</h1> </span>
+      <span className="font-bold text-lg my-4">@llin1 <h1 className="text-sm text-gray-400 font-thin">{!location?.address.county?location?.address.city:location?.address.county}</h1> </span>
       <span className="flex">
         <ul className="flex justify-around items-center w-max">
           <li className="mx-4 hover:text-gray-400 font-semibold text-md cursor-pointer ">
